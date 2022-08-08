4918cb8abd7b10972cbfb470500ba7f62ebf6945
Jason Gutierrez <jasongeee@yahoo.com>
2022-08-08 02:52:43 -0700

    inital commit

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+        require('autoprefixer'),
+        require('cssnano'),
+        require('postcss-nested')
+        // More postCSS modules here if needed
+    ]
+}
