be950e032896ce809de6bf573b502f321723ad37
Jason Gutierrez <jasongeee@yahoo.com>
2022-08-12 11:28:49 -0700

    fixed api call for specific cities

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import './styles/main.scss';
 
-
+const temp = document.querySelector('.temperature');
 
 console.log("Hello World");
+
+function getTemperature(data) {
+    document.querySelector('.temperature').innerHTML = data['main']['temp'];
+}
 
 // API Weather Function
 async function weatherAPI() {
     try {
-        const response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=Long Beach,CA,US&limit=5&appid=bc71a4a9ff59b7a45207d5b8012b790d', {mode: 'cors'});
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=long beach, us&appid=bc71a4a9ff59b7a45207d5b8012b790d&units=imperial', {mode: 'cors'});
         const weatherData = await response.json();
         console.log(weatherData);
     }
@@ -17,3 +21,4 @@ async function weatherAPI() {
 }
 
 weatherAPI();
+
